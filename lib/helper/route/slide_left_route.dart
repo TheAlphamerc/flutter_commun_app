a0276025607c8b2fdb64dbf64d88bdaa35c8b75e
Sonu Sharma <sonu.sharma045@gmail.com>
2021-06-20 19:55:10 +0530

    → Add sliding and fade routes

---
+++ lib/helper/route/slide_left_route.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SlideLeftRoute<T> extends MaterialPageRoute<T> {
+  SlideLeftRoute({WidgetBuilder builder, RouteSettings settings})
+      : super(builder: builder, settings: settings);
+  @override
+  Widget buildTransitions(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation, Widget child) {
+    return SlideTransition(
+      position: Tween<Offset>(
+        begin: const Offset(1.0, 0.0),
+        end: Offset.zero,
+      ).animate(
+          CurvedAnimation(parent: animation, curve: Curves.fastOutSlowIn)),
+      child: child,
+    );
+  }
+}
